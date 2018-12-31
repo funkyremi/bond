65eb1070e8d296316bcfd8e0678e342929d540c7
Rémi Testa <remi.testa@gmail.com>
2018-12-31 15:24:13 +0100

    Added polyfill for MediaRecorder and AudioContext

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import uniqueId from "lodash/uniqueId";
 import humanizeDuration from "humanize-duration";
 import dayjs from "dayjs";
-import 'bootstrap/dist/css/bootstrap.css';
-import createAudioMeter from "./methods";
+import "bootstrap/dist/css/bootstrap.css";
+import { AudioContext } from 'standardized-audio-context';
+import volumemeter from 'volume-meter'
 import "./App.css";
+window.MediaRecorder = require("audio-recorder-polyfill");
 
-const VOLUME_LIMIT = 0.01;
+const VOLUME_LIMIT = 5;
 const INTERVAL_WITHOUT_SOUND = 3000;
 
 class App extends Component {
@@ -17,90 +19,127 @@ class App extends Component {
       recordStartTime: null,
       recording: false
     };
-    this.chunks = [];
   }
   componentDidMount() {
     if (navigator.mediaDevices.getUserMedia) {
       const onSuccess = stream => {
-        const audioContext = new AudioContext();
-        const mediaStreamSource = audioContext.createMediaStreamSource(stream);
-        const meter = createAudioMeter(audioContext);
-        mediaStreamSource.connect(meter);
-        this.mediaRecorder = new MediaRecorder(stream);
-        this.mediaRecorder.ondataavailable = e => {
-          this.chunks.push(e.data);
-        };
-        let intervalWithoutSound = 0;
-        setInterval(() => {
+        // Volume meter
+        const context = new AudioContext();
+        const src = context.createMediaStreamSource(stream)
+        let lastSoundTime = new Date().getTime();
+        const meter = volumemeter(context, {}, (volume) => {
           if (this.state.recording) {
-            if (meter.volume < VOLUME_LIMIT) {
-              intervalWithoutSound += 100;
-              if (intervalWithoutSound === INTERVAL_WITHOUT_SOUND) {
-                if (
-                  new Date().getTime() - this.state.recordStartTime.getTime() <
-                  (INTERVAL_WITHOUT_SOUND + 1000)
-                ) {
-                  this.mediaRecorder.stop();
-                  this.setState({ recording: false, recordStartTime: null });
-                } else {
-                  this.stop();
-                }
-                intervalWithoutSound = 0;
+            if (volume < VOLUME_LIMIT) {
+              if (new Date().getTime() - lastSoundTime >= INTERVAL_WITHOUT_SOUND) {
+                this.stopRecording();
               }
             } else {
-              intervalWithoutSound = 0;
+              lastSoundTime = new Date().getTime();
             }
-          } else if (meter.volume > VOLUME_LIMIT) {
-            this.record();
+          } else if (volume > VOLUME_LIMIT && !this.playbackInProgress()) {
+            this.startRecording();
           }
-        }, 100);
+        });
+        stream.onended = meter.stop.bind(meter)
+        src.connect(meter)
+
+        // Recorder
+        this.mediaRecorder = new MediaRecorder(stream);
+        this.mediaRecorder.addEventListener("dataavailable", e => {
+          this.mediaRecorder.stream.getTracks().forEach(i => i.stop());
+          if ((new Date().getTime() - this.state.recordStartTime.getTime()) > (INTERVAL_WITHOUT_SOUND + 1000)) {
+            // Save the record if the duration is > 1s
+            const records = this.state.records;
+            const url = window.URL.createObjectURL(e.data);
+            records.unshift({
+              id: uniqueId(),
+              date: this.state.recordStartTime,
+              url,
+              paused: true,
+              recordStartTime: this.state.recordStartTime,
+              recordEndTime: new Date()
+            });
+            this.setState({
+              records,
+              recording: false,
+              recordStartTime: null
+            });
+          } else {
+            // Don't save the record if < 1s
+            this.setState({
+              recording: false,
+              recordStartTime: null
+            });
+          }
+        });
       };
       const onError = err => {
+        console.error(err);
       };
       navigator.mediaDevices
         .getUserMedia({ audio: true })
         .then(onSuccess, onError);
     } else {
+      console.error("Browser doesn't allow audio recording");
     }
   }
-  record() {
+  startRecording() {
     this.setState({
       recording: true,
       recordStartTime: new Date()
     });
-    this.chunks = [];
     this.mediaRecorder.start();
   }
-  stop() {
+  stopRecording() {
     this.mediaRecorder.stop();
-    const blob = new Blob(this.chunks, { type: "audio/wav" });
-    const records = this.state.records;
-    const url = window.URL.createObjectURL(blob);
-    records.unshift({
-      id: uniqueId(),
-      date: new Date(),
-      url,
-      recordStartTime: this.state.recordStartTime,
-      recordEndTime: new Date()
-    });
-    this.setState({
-      records,
-      recording: false,
-      recordStartTime: null
-    });
+  }
+  playbackInProgress() {
+    return this.state.records.find(record => !record.paused);
+  }
+  toggleRecord() {
+    if (this.state.recording === true) {
+      this.stopRecording();
+    } else {
+      this.startRecording();
+    }
   }
   getDuration(start, end) {
     return humanizeDuration(end - start, { round: true });
   }
   formatTime(date) {
-    return dayjs(date).format("HH:mm:ss");
+    return dayjs(date).format("DD/MM/YYYY HH:mm:ss");
+  }
+  removeRecord(id) {
+    const records = this.state.records;
+    const recordIndex = records.findIndex(record => record.id === id);
+    if (recordIndex > -1) {
+      records.splice(recordIndex, 1);
+      this.setState({ records });
+    }
   }
   togglePlayback(ref) {
     const element = this.refs[ref];
     if (!element.paused) {
       element.pause();
+      element.currentTime = 0;
     } else {
       element.play();
+    }
+  }
+  handlePlay(id) {
+    const records = this.state.records;
+    const recordIndex = records.findIndex(record => record.id === id);
+    if (recordIndex > -1) {
+      records[recordIndex].paused = false;
+      this.setState({ records });
+    }
+  }
+  handlePause(id) {
+    const records = this.state.records;
+    const recordIndex = records.findIndex(record => record.id === id);
+    if (recordIndex > -1) {
+      records[recordIndex].paused = true;
+      this.setState({ records });
     }
   }
   render() {
@@ -108,24 +147,60 @@ class App extends Component {
       <div className="App">
         <div className="container">
           <div id="bars">
-            <div className={`bar ${this.state.recording && 'bg-red'}`} />
-            <div className={`bar ${this.state.recording && 'bg-red'}`} />
-            <div className={`bar ${this.state.recording && 'bg-red'}`} />
-            <div className={`bar ${this.state.recording && 'bg-red'}`} />
-            <div className={`bar ${this.state.recording && 'bg-red'}`} />
-            <div className={`bar ${this.state.recording && 'bg-red'}`} />
-            <div className={`bar ${this.state.recording && 'bg-red'}`} />
-            <div className={`bar ${this.state.recording && 'bg-red'}`} />
-            <div className={`bar ${this.state.recording && 'bg-red'}`} />
-            <div className={`bar ${this.state.recording && 'bg-red'}`} />
+            <div className={`bar ${this.state.recording && "bg-red"}`} />
+            <div className={`bar ${this.state.recording && "bg-red"}`} />
+            <div className={`bar ${this.state.recording && "bg-red"}`} />
+            <div className={`bar ${this.state.recording && "bg-red"}`} />
+            <div className={`bar ${this.state.recording && "bg-red"}`} />
+            <div className={`bar ${this.state.recording && "bg-red"}`} />
+            <div className={`bar ${this.state.recording && "bg-red"}`} />
+            <div className={`bar ${this.state.recording && "bg-red"}`} />
+            <div className={`bar ${this.state.recording && "bg-red"}`} />
+            <div className={`bar ${this.state.recording && "bg-red"}`} />
           </div>
           <div>
             {this.state.records.map(record => {
               return (
-                <div className="record clickable" key={record.id} onClick={() => this.togglePlayback(record.id)}>
-                  <span className="float-left record-info"><span>{this.formatTime(record.date)}</span></span>
-                  <span className="float-right record-info"><span>{this.getDuration(record.recordStartTime, record.recordEndTime)}</span></span>
-                  <audio ref={record.id} id={record.id} src={record.url} />
+                <div className="record" key={record.id}>
+                  {record.paused ? (
+                    <i
+                      className="fas fa-2x fa-play-circle clickable float-left playback-button"
+                      title="Play"
+                      onClick={() => this.togglePlayback(record.id)}
+                    />
+                  ) : (
+                    <i
+                      className="fas fa-2x fa-pause-circle clickable float-left playback-button"
+                      title="Pause"
+                      onClick={() => this.togglePlayback(record.id)}
+                    />
+                  )}
+                  <span className="text-center">
+                    {this.formatTime(record.date)} (
+                    {this.getDuration(
+                      record.recordStartTime,
+                      record.recordEndTime
+                    )}
+                    )
+                  </span>
+                  <span title="Remove record" className="float-right clickable margin-left-sm" onClick={() => this.removeRecord(record.id)}>
+                    <i className="fas fa-lg fa-trash-alt" />
+                  </span>
+                  <span title="Download record" className="float-right">
+                    <a
+                      href={record.url}
+                      className="text-black"
+                      download={`record ${this.formatTime(record.date)}.wav`}
+                    >
+                      <i className="fas fa-lg fa-download" />
+                    </a>
+                  </span>
+                  <audio
+                    onPlay={() => this.handlePlay(record.id)}
+                    onPause={() => this.handlePause(record.id)}
+                    ref={record.id}
+                    src={record.url}
+                  />
                 </div>
               );
             })}
